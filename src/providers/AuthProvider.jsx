da5103c0c0f061c1db9ae2a81c4e6ad9bbf5cc53
amirulislam71 <amirultechon@gmail.com>
2023-06-10 02:59:34 +0600

    jsonwebtoken implement

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -44,8 +45,16 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currenUser) => {
-      setUser(currenUser);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((res) => {
+            localStorage.setItem("access-token", res.data.token);
+          });
+      } else localStorage.removeItem("access-token");
+
       setLoading(false);
     });
     return () => {
